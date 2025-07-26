ab65ac3fd5bb28c428423b526a94d95e6a8fb86e
Manuel <manuelmorenob7@gmail.com>
2025-07-26 17:24:47 -0300

    Initial commit

---
+++ server/minions.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const apiRouter = express.Router();
+
+const {
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+} = require("../db");
