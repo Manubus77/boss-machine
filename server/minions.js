8532c756e9e39bd9279176b01ccfe68dc19cdeff
Manuel <manuelmorenob7@gmail.com>
2025-07-28 16:35:54 -0300

    Added parameter middleware for ID validation and Finished GET methods for minions.js

--- server/minions.js
+++ server/minions.js
@@ -9,14 +9,26 @@ const {
   deleteFromDatabasebyId,
 } = require("./db");
 
-// GET all minions
+// Validating Minion ID and saving it to the mininonId param of the req object
+minionsRouter.param("minionId", (req, res, next, id) => {
+  const minion = getFromDatabaseById("minions", id);
+  if (minion) {
+    req.minion = minion;
+    next();
+  } else {
+    res.status(404).send({ error: "Minion not found" });
+  }
+});
 
+// GET all minions
 minionsRouter.get("/", (req, res, next) => {
   const allMinions = getAllFromDatabase("minions");
   res.status(200).send(allMinions);
 });
 
 // GET a single minion by id
-minionsRouter.get("/minionId", (req, res, next) => {});
+minionsRouter.get("/:minionId", (req, res, next) => {
+  res.status(200).send(req.minion);
+});
 
 module.exports = minionsRouter;
