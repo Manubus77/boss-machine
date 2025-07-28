b89e843b39a63e36c9695917ffd757f6470a5bef
Manuel <manuelmorenob7@gmail.com>
2025-07-28 18:22:58 -0300

    Updated DELETE to remove unnnecesary send message and PUT to check for errors

--- server/minions.js
+++ server/minions.js
@@ -32,7 +32,7 @@ minionsRouter.get("/:minionId", (req, res, next) => {
   res.status(200).send(req.minion);
 });
 
-//Middleware for checking if the req body of for PUT/POST verbs are valid
+//Middleware for checking if the req body of for PUT/POST request are valid
 const minionValidation = (req, res, next) => {
   try {
     isValidMinion(req.body);
@@ -52,14 +52,18 @@ minionsRouter.post("/", minionValidation, (req, res, next) => {
 minionsRouter.put("/:minionId", minionValidation, (req, res, next) => {
   req.body.id = req.params.minionId;
   const updatedMinion = updateInstanceInDatabase("minions", req.body);
-  res.status(200).send(updatedMinion);
+  if (updatedMinion) {
+    res.status(200).send(updatedMinion);
+  } else {
+    res.status(404).send({ error: "Minion not found or invalid" });
+  }
 });
 
 // DELETE a minion by ID
 minionsRouter.delete("/:minionId", (req, res, next) => {
   const deleted = deleteFromDatabasebyId("minions", req.params.minionId);
   if (deleted) {
-    res.status(204).send({ message: "Minion was deleted." });
+    res.status(204).send();
   } else {
     res.status(404).send({ error: "Minion not found" });
   }
