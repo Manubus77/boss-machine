a5dde277567978fe7dada0d21c3bdefebc220f3a
Manuel <manuelmorenob7@gmail.com>
2025-07-29 17:25:50 -0300

    Added POST a new meeting and DELETE all meetings in meetings.js

--- server/meetings.js
+++ server/meetings.js
@@ -7,12 +7,25 @@ const {
   addToDatabase,
   updateInstanceInDatabase,
   deleteFromDatabasebyId,
+  createMeeting,
 } = require("./db");
 
 //GET all meetings
 meetingsRouter.get("/", (req, res, next) => {
   const allMeetings = getAllFromDatabase("meetings");
   res.status(200).send(allMeetings);
+});
+
+//POST a new meeting
+meetingsRouter.post("/", (req, res, next) => {
+  const newMeeting = addToDatabase("meetings", createMeeting());
+  res.status(201).send(newMeeting);
+});
+
+//DELETE all meetings
+meetingsRouter.delete("/", (req, res, next) => {
+  deleteAllFromDatabase("meetings");
+  res.status(204).send();
 });
 
 module.exports = meetingsRouter;
